ded43bae52a62e67cc7690480a8dd94548eb0c5c
Srabonti Sen Neeti <105552240+SrabontiSenNeeti@users.noreply.github.com>
2022-05-16 21:50:12 +0600

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,23 @@
+print("Hello Neeti")
+print(5)
+print(5.0)
+print("1998")
+
+
+x=5
+print(x)
+
+y="Hello World"
+print(y)
+
+#This is Comment
+"""this
+is 
+multiple line comment"""
+
+x=6
+y=10
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
